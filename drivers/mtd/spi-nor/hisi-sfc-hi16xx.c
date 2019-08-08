8fcae57a49abef34d346f8de6be8c32e8812fa4f
John Garry <john.garry@huawei.com>
2019-08-08 13:43:07 +0100

    continued work

--- drivers/mtd/spi-nor/hisi-sfc-hi16xx.c
+++ drivers/mtd/spi-nor/hisi-sfc-hi16xx.c
@@ -26,6 +26,8 @@
 #define CMD_CONFIG_CMD_RW_MSK (1 << CMD_CONFIG_CMD_RW_OFF)
 #define CMD_CONFIG_CMD_DATA_EN_OFF 7
 #define CMD_CONFIG_CMD_DATA_EN_MSK (1 << CMD_CONFIG_CMD_DATA_EN_OFF)
+#define CMD_CONFIG_CMD_DUMMY_CNT_OFF 4
+#define CMD_CONFIG_CMD_DUMMY_CNT_MSK (0x7 << CMD_CONFIG_CMD_DUMMY_CNT_OFF)
 #define CMD_CONFIG_CMD_ADDR_EN_OFF 3
 #define CMD_CONFIG_CMD_ADDR_EN_MSK (1 << CMD_CONFIG_CMD_ADDR_EN_OFF)
 #define CMD_CONFIG_CMD_CS_SEL_OFF 1
@@ -106,14 +108,14 @@ static void hisi_spi_hi16xx_nor_init(struct hifmc_host *host)
 
 static int hisi_spi_hi16xx_nor_prep(struct spi_nor *nor, enum spi_nor_ops ops)
 {
-	pr_err("%s nor=%pS ops=%x\n", __func__, nor, ops);
+//	pr_err("%s nor=%pS ops=%x\n", __func__, nor, ops);
 
 	return 0;
 }
 
 static void hisi_spi_hi16xx_nor_unprep(struct spi_nor *nor, enum spi_nor_ops ops)
 {
-	pr_err("%s nor=%pS ops=%d\n", __func__, nor, ops);
+//	pr_err("%s nor=%pS ops=%d\n", __func__, nor, ops);
 }
 
 __maybe_unused static int hisi_spi_hi16xx_nor_op_reg(struct spi_nor *nor,
@@ -129,7 +131,8 @@ static int hisi_spi_hi16xx_nor_read_reg(struct spi_nor *nor, u8 opcode, u8 *buf,
 {
 	struct hifmc_priv *priv = nor->priv;
 	struct hifmc_host *host = priv->host;
-	u32 config, ins, addr, version, cmd_buf0, cmd_buf1, cmd_buf[2];
+	u32 config, ins, addr, version;
+	__le32 cmd_buf0, cmd_buf1, cmd_buf2, cmd_buf3,  cmd_buf[5];
 	int i;
 	static int count;
 
@@ -141,16 +144,13 @@ static int hisi_spi_hi16xx_nor_read_reg(struct spi_nor *nor, u8 opcode, u8 *buf,
 	cmd_buf1 = readl(host->regbase + CMD_DATABUF(1));
 
 
-	pr_err("%s nor=%pS opcode=0x%x buf=%pS len=%d host=%pS count=%d\n", __func__, nor, opcode, buf, len, host, count);
+//	pr_err("%s nor=%pS opcode=0x%x buf=%pS len=%d host=%pS count=%d\n", __func__, nor, opcode, buf, len, host, count);
 //	pr_err("%s1 nor=%pS config=0x%x ins=0x%x addr=0x%x version=0x%x cmd_buf0=0x%x cmd_buf1=0x%x\n",
 //		__func__, nor, config, ins, addr, version, cmd_buf0, cmd_buf1);
 
-	if (count >= 1)
-		return -1;
-
 	config &= ~CMD_CONFIG_DATA_CNT_MSK & ~CMD_CONFIG_CMD_CS_SEL_MSK &
 			~CMD_CONFIG_CMD_ADDR_EN_MSK & ~CMD_CONFIG_CMD_RW_MSK;
-	config |= len << CMD_CONFIG_DATA_CNT_OFF | CMD_CONFIG_CMD_DATA_EN_MSK |
+	config |= ((len +1 )<< CMD_CONFIG_DATA_CNT_OFF) | CMD_CONFIG_CMD_DATA_EN_MSK |
 			CMD_CONFIG_CMD_START_MSK;
 
 	writel(opcode, host->regbase + CMD_INS);
@@ -168,11 +168,15 @@ static int hisi_spi_hi16xx_nor_read_reg(struct spi_nor *nor, u8 opcode, u8 *buf,
 
 	cmd_buf0 = readl(host->regbase + CMD_DATABUF(0));
 	cmd_buf1 = readl(host->regbase + CMD_DATABUF(1));
-	cmd_buf[0] = cmd_buf0;
-	cmd_buf[1] = cmd_buf1;
+	cmd_buf2 = readl(host->regbase + CMD_DATABUF(2));
+	cmd_buf3 = readl(host->regbase + CMD_DATABUF(3));
+	cmd_buf[0] = le32_to_cpu(cmd_buf0);
+	cmd_buf[1] = le32_to_cpu(cmd_buf1);
+	cmd_buf[2] = le32_to_cpu(cmd_buf2);
+	cmd_buf[3] = le32_to_cpu(cmd_buf3);
 
-//	pr_err("%s4 nor=%pS config=0x%x ins=0x%x addr=0x%x version=0x%x cmd_buf0=0x%x cmd_buf1=0x%x\n",
-//		__func__, nor, config, ins, addr, version, cmd_buf[0], cmd_buf[1]);
+//	pr_err("%s4 nor=%pS config=0x%x ins=0x%x addr=0x%x version=0x%x cmd_buf0=0x%x cmd_buf[0]=0x%x\n",
+//		__func__, nor, config, ins, addr, version, cmd_buf0, cmd_buf[0]);
 
 	for (i = 0; i<len;i++) {
 		u8 *byte = (u8 *)&cmd_buf[0];
@@ -202,15 +206,18 @@ __maybe_unused static int hisi_spi_hi16xx_nor_dma_transfer(struct spi_nor *nor, 
 	return 0;
 }
 
+#define MAX_CMD_WORD 4
+
 static ssize_t hisi_spi_hi16xx_nor_read(struct spi_nor *nor, loff_t from, size_t len,
 		u_char *read_buf)
 {
 	struct hifmc_priv *priv = nor->priv;
 	struct hifmc_host *host = priv->host;
-	u32 config, ins, addr, version, cmd_buf0, cmd_buf1, cmd_bufx;
+	u32 config, ins, addr, version, cmd_buf0, cmd_buf1;
 	int i;
 	static int count;
-	
+	int remaining = len;
+	//WARN_ON_ONCE(1);
 	config = readl(host->regbase + CMD_CONFIG);
 	ins = readl(host->regbase + CMD_INS);
 	addr = readl(host->regbase + CMD_ADDR);
@@ -219,40 +226,64 @@ static ssize_t hisi_spi_hi16xx_nor_read(struct spi_nor *nor, loff_t from, size_t
 	cmd_buf1 = readl(host->regbase + CMD_DATABUF(1));
 	
 	
-	pr_err("%s nor=%pS read_buf=%pS len=%ld host=%pS count=%d\n", __func__, nor, read_buf, len, host, count);
+	pr_err("%s nor=%pS read_buf=%pS len=%ld host=%pS count=%d read opcode=0x%x addr=0x%x read_dummy=%x\n", __func__, nor, read_buf, len, host, count, nor->read_opcode, addr, nor->read_dummy);
 	//	pr_err("%s1 nor=%pS config=0x%x ins=0x%x addr=0x%x version=0x%x cmd_buf0=0x%x cmd_buf1=0x%x\n",
 	//		__func__, nor, config, ins, addr, version, cmd_buf0, cmd_buf1);
-	len = 16;
+
 	if (count >= 1)
 		return -1;
-	
-	config &= ~CMD_CONFIG_DATA_CNT_MSK & ~CMD_CONFIG_CMD_CS_SEL_MSK;
-	config |= ((len -1) << CMD_CONFIG_DATA_CNT_OFF) | CMD_CONFIG_CMD_DATA_EN_MSK |
-		    CMD_CONFIG_CMD_ADDR_EN_MSK |
-			CMD_CONFIG_CMD_START_MSK | CMD_CONFIG_CMD_RW_MSK;// 1: READ
-	writel(from, host->regbase + CMD_ADDR);
-	writel(0x3, host->regbase + CMD_INS);
-	writel(config, host->regbase + CMD_CONFIG);
 
-	pr_err("%s1nor=%pS read_buf=%pS len=%ld host=%pS count=%d config=0x%x\n", __func__, nor, read_buf, len, host, count, config);
+	do {
+		int read_len;
+
+		if (remaining > MAX_CMD_WORD * 4)
+			read_len = MAX_CMD_WORD * 4;
+		else
+			read_len = remaining;
+
+		remaining -= read_len;
+
+		config &= ~CMD_CONFIG_DATA_CNT_MSK & ~CMD_CONFIG_CMD_CS_SEL_MSK &
+				~CMD_CONFIG_CMD_DATA_EN_OFF;
+		config |= ((read_len + 1) << CMD_CONFIG_DATA_CNT_OFF) | CMD_CONFIG_CMD_DATA_EN_MSK |
+			    CMD_CONFIG_CMD_ADDR_EN_MSK |
+				(nor->read_dummy / 8) << CMD_CONFIG_CMD_DUMMY_CNT_OFF |
+				CMD_CONFIG_CMD_START_MSK | CMD_CONFIG_CMD_RW_MSK;// 1: READ
+		writel(from, host->regbase + CMD_ADDR);
+		writel(nor->read_opcode, host->regbase + CMD_INS);
+		writel(config, host->regbase + CMD_CONFIG);
+
+		pr_err("%s1 nor=%pS read_buf=%pS len=%ld host=%pS count=%d config=0x%x read_len=%x remaining=%d\n", __func__, nor, read_buf, len, host, count, config, read_len, remaining);
 
 sleep:
-	msleep(100);
+		msleep(100);
 
-	config = readl(host->regbase + CMD_CONFIG);
-	if (config & CMD_CONFIG_CMD_START_MSK)
-		goto sleep;
+		config = readl(host->regbase + CMD_CONFIG);
+		addr = readl(host->regbase + CMD_ADDR);
 
-	pr_err("%s2 nor=%pS read_buf=%pS len=%ld host=%pS count=%d config=0x%x\n", __func__, nor, read_buf, len, host, count, config);
+		if (config & CMD_CONFIG_CMD_START_MSK)
+			goto sleep;
 
-	for (i=0;i<len/4;i++) {
-		cmd_bufx = readl(host->regbase + CMD_DATABUF(i));
-		pr_err("%s3 i=%d cmd_bufx=0x%x\n", __func__, i, cmd_bufx);
-	}
-		
+		pr_err("%s2 nor=%pS read_buf=%pS len=%ld host=%pS count=%d config=0x%x addr=0x%x\n", __func__, nor, read_buf, len, host, count, config, addr);
 
-	return 16;
- 
+		for (i=0;i<2;i++) {
+			u32 cmd_bufx = readl(host->regbase + CMD_DATABUF(i));
+			u32 cmd_bufy = __swab32(cmd_bufx);
+			u8 *ptr = (u8 *)&cmd_bufy;
+			u8 aa, bb, cc, dd;
+
+			pr_err("%s3.0 i=%d cmd_bufx=0x%x cmd_bufy=0x%x\n", __func__, i, cmd_bufx, cmd_bufy);
+
+			*read_buf = aa = ptr[0];read_buf++;
+			*read_buf = bb = ptr[1];read_buf++;
+			*read_buf = cc = ptr[2];read_buf++;
+			*read_buf = dd = ptr[3];read_buf++;
+			
+			pr_err("%s3.1 i=%d cmd_bufx=0x%x [%02x %02x %02x %02x]\n", __func__, i, cmd_bufx, aa, bb, cc, dd);
+		}
+	}while (0);
+	pr_err("%s out nor=%pS returning len=%ld\n", __func__, nor, len);
+	return len;
 }
 
 static ssize_t hisi_spi_hi16xx_nor_write(struct spi_nor *nor, loff_t to,
