59e4e420bce8fc7bec4c2f5355e9a682d62a58e8
John Garry <john.garry@huawei.com>
2020-11-02 13:34:38 +0000

    add test for CMDQ_PROD_LOCKED_FLAG unset

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -1504,7 +1504,7 @@ static int arm_smmu_cmdq_issue_cmdlist(struct arm_smmu_device *smmu,
 	ktime_t initial, final, *t;//, j_timeout;
 	int cpu;
 //	int loop_count = 0;
-	u32 old2 = 0;
+//	u32 old2 = 0;
 //	u32 old = 0;
 
 	/* 1. Allocate some space in the queue */
@@ -1528,6 +1528,7 @@ static int arm_smmu_cmdq_issue_cmdlist(struct arm_smmu_device *smmu,
 	//	}
 	//	BUG_ON(ktime_after(ktime_get(), j_timeout));
 
+		atomic_cond_read_relaxed(&cmdq->q.llq.atomic.prod, !(VAL & CMDQ_PROD_LOCKED_FLAG));
 
 		llq.prod = xchg(&cmdq->q.llq.prod, CMDQ_PROD_LOCKED_FLAG);
 		//if (loop_count < 20 || loop_count > 995)
@@ -1558,7 +1559,8 @@ static int arm_smmu_cmdq_issue_cmdlist(struct arm_smmu_device *smmu,
 
 //		if (atomic64_read(&jtries) < 20)
 //			pr_err("%s3 cpu%d exiting loop setting head @ head.prod=0x%x, llq.prod=0x%x\n", __func__, cpu, head.prod, llq.prod);
-		old2 = xchg(&cmdq->q.llq.prod, head.prod);
+//		old2 = xchg(&cmdq->q.llq.prod, head.prod);
+		WRITE_ONCE(cmdq->q.llq.prod, head.prod);
 		
 		break;
 	} while (1);
