ddf7591d543bffe10b3d9403931ebdefc054e23f
John Garry <john.garry@huawei.com>
2021-04-19 14:42:00 +0100

    mq=deadline by defaukt

--- block/elevator.c
+++ block/elevator.c
@@ -621,8 +621,8 @@ static inline bool elv_support_iosched(struct request_queue *q)
  */
 static struct elevator_type *elevator_get_default(struct request_queue *q)
 {
-	if (q->nr_hw_queues != 1)
-		return NULL;
+//	if (q->nr_hw_queues != 1)
+//		return NULL;
 
 	return elevator_get(q, "mq-deadline", false);
 }
