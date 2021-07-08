db022860a72e9bcc7c5594ff9f4dcd461a4a7227
John Garry <john.garry@huawei.com>
2021-07-08 14:56:26 +0100

    continued work

--- block/blk-mq-sched.c
+++ block/blk-mq-sched.c
@@ -594,7 +594,6 @@ static int blk_mq_init_sched_shared_sbitmap(struct request_queue *queue)
 	if (ret)
 		goto err_rqs;
 
-	/* Just assign them all -the only thing we change is the sbitmap size when we want to change request queue depth */
 	queue_for_each_hw_ctx(queue, hctx, i) {
 		for (j = 0; j < queue->nr_requests; j++)
 			hctx->sched_tags->static_rqs[j] = queue->static_rqs[j];
