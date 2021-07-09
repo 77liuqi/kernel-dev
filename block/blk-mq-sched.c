12440ff559c48cc89a32a4c2fcd3a4654130420b
John Garry <john.garry@huawei.com>
2021-07-09 12:51:23 +0100

    a fix?

--- block/blk-mq-sched.c
+++ block/blk-mq-sched.c
@@ -710,7 +710,10 @@ void blk_mq_sched_free_requests(struct request_queue *q)
 {
 	struct blk_mq_hw_ctx *hctx;
 	int i;
-		
+
+	if (blk_mq_is_sbitmap_shared(q->tag_set->flags))
+		return;
+
 	queue_for_each_hw_ctx(q, hctx, i) {
 		if (hctx->sched_tags)
 			blk_mq_free_rqs(q->tag_set, hctx->sched_tags, i);
