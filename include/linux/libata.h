f09feec5e235ee755439778d64fb5c37eca872aa
John Garry <john.garry@huawei.com>
2021-11-12 15:55:59 +0000

    verbose debug

--- include/linux/libata.h
+++ include/linux/libata.h
@@ -39,8 +39,8 @@
  * compile-time options: to be removed as soon as all the drivers are
  * converted to the new debugging mechanism
  */
-#undef ATA_DEBUG		/* debugging output */
-#undef ATA_VERBOSE_DEBUG	/* yet more debugging output */
+#define ATA_DEBUG		/* debugging output */
+#define ATA_VERBOSE_DEBUG	/* yet more debugging output */
 #undef ATA_IRQ_TRAP		/* define to ack screaming irqs */
 #undef ATA_NDEBUG		/* define to disable quick runtime checks */
 
